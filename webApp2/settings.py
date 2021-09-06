1f0412d894250fc44e634cc25e9ee1e82ea96011
OriginalCyber <59059908+OriginalCyber@users.noreply.github.com>
2021-09-06 18:47:08 +0700

    prepare deploy to heroku

--- webApp2/settings.py
+++ webApp2/settings.py
@@ -118,14 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'assets' 
 STATICFILES_DIRS = [
     BASE_DIR / 'statics'
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
